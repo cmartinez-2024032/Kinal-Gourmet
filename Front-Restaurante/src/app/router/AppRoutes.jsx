7220878d99a00aecbf055ab72acefe3d336adef2
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-04-28 21:13:27 -0600

    Se ordeno la estructura de rutas para asi no perderse al momento de ver codigo

--- Front-Restaurante/src/app/router/AppRoutes.jsx
+++ Front-Restaurante/src/app/router/AppRoutes.jsx
@@ -1,27 +1,39 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { AuthPage } from '../../features/auth/pages/AuthPage'
-import { VerifyPage } from '../../features/auth/pages/VerifyPage'
-import { DashboardPage } from '../pages/DashboardPage'
+import { Routes, Route } from 'react-router-dom'
+
+// Auth
+import { AuthPage }      from '../../features/auth/pages/AuthPage'
+import { VerifyPage }    from '../../features/auth/pages/VerifyPage'
+import { RegisterForm }  from '../../features/auth/components/RegisterForm'
+import { LandingPage }   from '../../features/auth/pages/LandingPage'
+
+// Dashboard
+import { DashboardPage }   from '../pages/DashboardPage'
 import { DashboardLayout } from '../layouts/DashboardLayout'
+
+// Admin General
+import { AdminGeneralLayout } from '../../features/admin-general/layout/AdminGeneralLayout'
+import { AdminGeneralPage }   from '../../features/admin-general/pages/AdminGeneralPage.jsx'
+import { RestaurantesPage }   from '../../features/admin-general/pages/AdminRestaurantesPage.jsx'
+
+// Admin Restaurante
+import { AdminRestLayout } from '../../features/admin-restaurante/layout/AdminRestLayout.jsx'
+import { PlatilloPage }    from '../../features/admin-restaurante/pages/PlatilloPage.jsx'
+import { MesaPage }        from '../../features/admin-restaurante/pages/MesaPage.jsx'
+
 import { PrivateRoute } from './PrivateRoute'
 
-import { RegisterForm } from '../../features/auth/components/RegisterForm'
-import { LandingPage } from '../../features/auth/pages/LandingPage'
-import{AdminGeneralPage} from '../../features/admin-general/pages/AdminGeneralPage.jsx'
-import { AdminGeneralLayout } from '../../features/admin-general/layout/AdminGeneralLayout'
-import { RestaurantesPage } from '../../features/admin-general/pages/AdminRestaurantesPage.jsx'
-
-import { AdminRestLayout } from '../../features/admin-restaurante/layout/AdminRestLayout.jsx'
-import {PlatilloPage} from '../../features/admin-restaurante/pages/PlatilloPage.jsx'
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<LandingPage />} /> 
-            <Route path="*" element={<LandingPage />} /> 
-            <Route path="/login" element={<AuthPage />} />
-            <Route path="/register" element={<AuthPage><RegisterForm /></AuthPage>} />
-            <Route path="/verify/:token" element={<VerifyPage />} />
 
+            {/* Públicas */}
+            <Route path="/"               element={<LandingPage />} />
+            <Route path="*"               element={<LandingPage />} />
+            <Route path="/login"          element={<AuthPage />} />
+            <Route path="/register"       element={<AuthPage><RegisterForm /></AuthPage>} />
+            <Route path="/verify/:token"  element={<VerifyPage />} />
+
+            {/* Dashboard */}
             <Route
                 path="/dashboard"
                 element={
@@ -33,6 +45,7 @@ export const AppRoutes = () => {
                 }
             />
 
+            {/* Admin General */}
             <Route
                 path="/adminGeneral"
                 element={
@@ -41,10 +54,11 @@ export const AppRoutes = () => {
                     </PrivateRoute>
                 }
             >
-                <Route index element={<AdminGeneralPage />} />
+                <Route index              element={<AdminGeneralPage />} />
                 <Route path="restaurantes" element={<RestaurantesPage />} />
             </Route>
 
+            {/* Admin Restaurante */}
             <Route
                 path="/adminRestaurante"
                 element={
@@ -53,8 +67,9 @@ export const AppRoutes = () => {
                     </PrivateRoute>
                 }
             >
-                <Route index element={<div>Resumen</div>} />
+                <Route index           element={<div>Resumen</div>} />
                 <Route path="platillos" element={<PlatilloPage />} />
+                <Route path="mesas"     element={<MesaPage />} />
             </Route>
 
         </Routes>
