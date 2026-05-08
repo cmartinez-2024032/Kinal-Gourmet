20c29a3ca0503d73231aa1151a881e980dae4d1c
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-05-08 11:34:01 -0600

    Se arreglo el controller y routes de backend

--- Backend-Restaurante/Kinal-Gourmet-House/src/reservations/reservation.routes.js
+++ Backend-Restaurante/Kinal-Gourmet-House/src/reservations/reservation.routes.js
@@ -1,18 +1,10 @@
 import { Router } from "express";
-import {
-  createReservation,
-  getReservations,
-  getReservationById,
-  updateReservation,
-  deleteReservation,
-  updateReservationStatus
-} from "./reservation.controller.js";
+import { createReservation, getReservations, getReservationById, updateReservation, deleteReservation } from "./reservation.controller.js";
 import { verifyToken } from "../../middlewares/auth-integration.middleware.js";
-import { belongsToRestaurant } from "../../middlewares/role.middleware.js";
+import { isRestaurantAdmin, isPlatformAdmin, belongsToRestaurant } from "../../middlewares/role.middleware.js";
 
 const router = Router();
 
-// Todas las rutas requieren autenticación
 router.use(verifyToken);
 
 router.post('/create', belongsToRestaurant, createReservation);
@@ -22,9 +14,6 @@ router.get('/', getReservations);
 router.get('/:id', getReservationById);
 
 router.put('/:id', belongsToRestaurant, updateReservation);
-
-// FIX: era "/reservations/:id/status" → ruta duplicada, corregida a "/:id/status"
-router.patch('/:id/status', updateReservationStatus);
 
 router.delete('/:id', deleteReservation);
 
